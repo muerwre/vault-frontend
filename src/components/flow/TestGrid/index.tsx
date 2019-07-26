0e8d42095d15406912e60900244014d1ee49cd37
muerwre <gotham48@gmail.com>
2019-07-26 14:23:19 +0700

    fixed grid

--- src/components/flow/TestGrid/index.tsx
+++ src/components/flow/TestGrid/index.tsx
@@ -2,21 +2,33 @@ import * as React from 'react';
 import { Cell } from "~/components/flow/Cell";
 import { range } from 'ramda';
 
-const style = require('./style.scss');
+import * as styles from './styles.scss';
 
 export const TestGrid = () => (
-  <div className={style.grid_test}>
-    <Cell
-      height={1}
-      width={4}
-      title="Example cell Example cell Example cell Example cell Example cell Example cell Example cell "
-      is_hero
-    />
+  <div className={styles.grid_test}>
+    <div style={{
+      gridRow: '1 / 2',
+      gridColumn: '1 / -1',
+      background: 'green',
+    }}
+    >
+      HERO
+    </div>
+
+    <div style={{
+      gridRow: '2 / 4',
+      gridColumn: '-2 / -1',
+      background: 'blue',
+    }}
+    >
+      STAMP
+    </div>
+
     {
       range(1,20).map(el => (
         <Cell
-          width={1}
-          height={1}
+          width={Math.floor(Math.random() * 3)}
+          height={Math.floor(Math.random() * 3)}
           title="Example cell Example cell Example cell Example cell Example cell Example cell Example cell "
         />
       ))
