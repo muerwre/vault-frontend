a2bb40bb146457ecc9f08042ee31bb0facfea9eb
muerwre <gotham48@gmail.com>
2019-08-01 04:01:16 +0700

    fixed editor layout

--- src/containers/examples/EditorExample/index.tsx
+++ src/containers/examples/EditorExample/index.tsx
@@ -18,67 +18,58 @@ interface IProps {}
 
 const EditorExample: FC<IProps> = () => (
     <Card className={styles.wrap} seamless>
-      <Group seamless style={{ flex: 1 }}>
-        <Panel>
-          <Group horizontal>
-            <Filler>TITLE</Filler>
-            <div className={styles.close_icon} />
-          </Group>
-        </Panel>
+      <Group horizontal className={styles.group} seamless>
+        <div className={styles.editor}>
+          <Panel>
+            <TextInput onChange={console.log} label="Название" value="Значение" />
+          </Panel>
 
-        <Group horizontal className={styles.group} seamless>
-          <div className={styles.editor}>
-            <Panel>
-              <TextInput onChange={console.log} label="Название" value="Значение" />
-            </Panel>
+          <Panel>
+            <InputText title="Заголовок" />
+          </Panel>
 
-            <Panel>
-              <InputText title="Заголовок" />
-            </Panel>
+          <Panel className={classNames(styles.editor_panel, styles.editor_image_panel)}>
+            <Scroll>
+              <CellGrid className={styles.editor_image_container} size={200}>
+                <div className={styles.editor_image} />
+                <div className={styles.editor_image} />
+                <div className={styles.editor_image} />
+                <div className={styles.editor_image} />
+              </CellGrid>
+            </Scroll>
+          </Panel>
+        </div>
 
-            <Panel className={classNames(styles.editor_panel, styles.editor_image_panel)}>
-              <Scroll>
-                <CellGrid className={styles.editor_image_container} size={200}>
-                  <div className={styles.editor_image} />
-                  <div className={styles.editor_image} />
-                  <div className={styles.editor_image} />
-                  <div className={styles.editor_image} />
-                </CellGrid>
-              </Scroll>
-            </Panel>
-          </div>
+        <div className={styles.panel}>
+          <Filler>
+            <Padder>
+              <Group>
+                <Card className={styles.feature_card}>Layout setup</Card>
 
-          <div className={styles.panel}>
-            <Filler>
-              <Padder>
-                <Group>
-                  <Card className={styles.feature_card}>Layout setup</Card>
+                <Card className={styles.feature_card}>Cover changer</Card>
 
-                  <Card className={styles.feature_card}>Cover changer</Card>
+                <Card className={styles.feature_card}>Track</Card>
 
-                  <Card className={styles.feature_card}>Track</Card>
+                <Tags
+                  tags={[
+                    { title: 'Избранный', feature: 'red' },
+                    { title: 'Плейлист', feature: 'green' },
+                    { title: 'Просто' },
+                    { title: '+ фото', feature: 'black' },
+                    { title: '+ с музыкой', feature: 'black' },
+                  ]}
+                />
 
-                  <Tags
-                    tags={[
-                      { title: 'Избранный', feature: 'red' },
-                      { title: 'Плейлист', feature: 'green' },
-                      { title: 'Просто' },
-                      { title: '+ фото', feature: 'black' },
-                      { title: '+ с музыкой', feature: 'black' },
-                    ]}
-                  />
+                <Filler />
 
-                  <Filler />
+              </Group>
+            </Padder>
+          </Filler>
 
-                </Group>
-              </Padder>
-            </Filler>
-
-            <Panel>
-              <Button>Submit?</Button>
-            </Panel>
-          </div>
-        </Group>
+          <Panel>
+            <Button>Submit?</Button>
+          </Panel>
+        </div>
       </Group>
     </Card>
 );
