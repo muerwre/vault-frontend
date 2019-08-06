a977c73449a18dcff8f41d1c528df934fc936aa4
muerwre <gotham48@gmail.com>
2019-08-06 19:09:07 +0700

    ScrollDialog error handling

--- src/redux/modal/reducer.ts
+++ src/redux/modal/reducer.ts
@@ -10,7 +10,7 @@ export interface IModalState {
 
 const INITIAL_STATE: IModalState = {
   is_shown: true,
-  dialog: DIALOGS.EDITOR,
+  dialog: DIALOGS.LOGIN,
 };
 
 export default createReducer(INITIAL_STATE, MODAL_HANDLERS);
