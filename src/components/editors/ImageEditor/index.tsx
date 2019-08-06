ee17b72a7fb1c015bd79d594e4357650d1180fb1
muerwre <gotham48@gmail.com>
2019-08-06 18:55:19 +0700

    started editor

---
+++ src/components/editors/ImageEditor/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { INode } from '~/redux/types';
+import * as styles from './styles.scss';
+
+interface IProps {
+  data: INode,
+  setData: (val: INode) => void;
+};
+
+const ImageEditor: FC<IProps> = ({
+}) => (
+    <div className={styles.uploads} />
+  )
+
+export { ImageEditor };
--- src/containers/dialogs/EditorDialog/index.tsx
+++ src/containers/dialogs/EditorDialog/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useCallback } from 'react';
 import { ScrollDialog } from '../ScrollDialog';
 import { IDialogProps } from '~/redux/modal/constants';
 import { useCloseOnEscape } from '~/utils/hooks';
@@ -6,22 +6,26 @@ import { Group } from '~/components/containers/Group';
 import { InputText } from '~/components/input/InputText';
 import { Button } from '../../../components/input/Button/index';
 import { Padder } from '~/components/containers/Padder';
-import * as styles from '~/containers/examples/HorizontalExample/styles.scss';
+import * as styles from './styles.scss';
 import { connect } from 'react-redux';
 import { selectNode } from '~/redux/node/selectors';
+import { ImageEditor } from '~/components/editors/ImageEditor';
 
 const mapStateToProps = selectNode;
 const mapDispatchToProps = {};
 
-type IProps = IDialogProps & {};
+type IProps = IDialogProps & ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps & {};
 
-const EditorDialogUnconnected: FC<IProps> = ({ onRequestClose }) => {
-  const title = <div>title</div>;
+const EditorDialogUnconnected: FC<IProps> = ({ onRequestClose, editor }) => {
+  const [data, setData] = useState(editor);
+  const setTitle = useCallback(title => {
+    setData({ ...data, title });
+  }, [setData, data]);
 
   const buttons = (
     <Padder>
       <Group horizontal>
-        <InputText title="Название" />
+        <InputText title="Название" value={data.title} handler={setTitle} />
 
         <Button title="Сохранить" iconRight="check" />
       </Group>
@@ -32,13 +36,11 @@ const EditorDialogUnconnected: FC<IProps> = ({ onRequestClose }) => {
 
   return (
     <ScrollDialog buttons={buttons} width={860}>
-      <div className={styles.uploads}>
-        <div className={styles.cell} />
-        <div className={styles.cell} />
-        <div className={styles.cell} />
-        <div className={styles.cell} />
-        <div className={styles.cell} />
-        <div className={styles.cell} />
+      <div className={styles.editor}>
+        <ImageEditor
+          data={data}
+          setData={setData}
+        />
       </div>
     </ScrollDialog>
   );
