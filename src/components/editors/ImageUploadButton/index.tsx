ec964ceea415fddc798c51e29ead63d256eb8348
muerwre <gotham48@gmail.com>
2019-08-08 18:43:19 +0700

    upload button plus

--- src/components/editors/ImageUploadButton/index.tsx
+++ src/components/editors/ImageUploadButton/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC, ChangeEventHandler } from 'react';
 import * as styles from './styles.scss';
+import { Icon } from '~/components/input/Icon';
 
 interface IProps {
   onUpload?: ChangeEventHandler<HTMLInputElement>;
@@ -10,6 +11,9 @@ const ImageUploadButton: FC<IProps> = ({
 }) => (
     <div className={styles.wrap}>
       <input type="file" onChange={onUpload} />
+      <div className={styles.icon}>
+        <Icon size={32} icon="plus" />
+      </div>
     </div>
   )
 
