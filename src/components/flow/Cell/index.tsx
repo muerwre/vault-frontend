f922536ffa0b2bb76e19e846e9658ae0cd9abacf
muerwre <gotham48@gmail.com>
2019-08-19 17:27:45 +0700

    added text nodes

--- src/components/flow/Cell/index.tsx
+++ src/components/flow/Cell/index.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import * as styles from './styles.scss';
+import { TEXTS } from '~/constants/texts';
 
 import classNames = require('classnames');
 
@@ -9,6 +10,7 @@ interface IProps {
   title?: string;
   is_hero?: boolean;
   is_stamp?: boolean;
+  is_text?: boolean;
 }
 
 const Cell: FC<IProps> = ({
@@ -16,14 +18,22 @@ const Cell: FC<IProps> = ({
   height = 1,
   title,
   is_hero,
+  is_text = (Math.random() > 0.8),
 }) => (
   <div
-    className={classNames(styles.cell, `vert-${height}`, `hor-${width}`)}
+    className={
+      classNames(
+        styles.cell, 
+        `vert-${height}`,
+        `hor-${width}`,
+        { is_text },
+      )}
     style={{
       // gridRowEnd: `span ${height}`,
       // gridColumnEnd: `span ${width}`,
     }}
   >
+    {is_text && <div className={styles.text}>{TEXTS.LOREM_IPSUM}</div>}
     { title && <div className={styles.title}>{title}</div> }
   </div>
 );
