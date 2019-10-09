27cf737e0b4b5af490b1d164c8af7b2f50aaf3e9
Fedor Katurov <gotham48@gmail.com>
2019-10-09 17:11:25 +0700

    added todos

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,45 @@
 /*
   sortable grid: http://clauderic.github.io/react-sortable-hoc/#/basic-configuration/grid?_k=hjqdj1
+  
+  [BUGS]
+  
+  todo: flow hangs on empty response
+  
+  [CONTENT]
+
+  todo: adding photos to comments
+  todo: adding media to comments
+  todo: adding media to posts
+  todo: adding text to posts
+  todo: covers for posts
+  todo: wallpapers for posts
+  todo: changing flow appearance for posts
+
+  todo: display for all node types
+  todo: related by tags
+  todo: sticky left column
+  todo: adaptive relates count
+
+  todo: boris page
+  todo: user profile and settings
+
+  [IMPORT]
+
+  todo: import users properly
+  todo: auth using md5 fallback
+  todo: import files properly
+  todo: import nodes properly
+  todo: import comments properly
+  todo: convert comment content properly
+  todo: convert post text properly
+
+  [NOT TODAY]
+
+  todo: swipable image slideshow
+  todo: notifications
+  todo: PMs
+  todo: 
+
 */
 import * as React from 'react';
 import { render } from 'react-dom';
