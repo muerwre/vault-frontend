d676ff88e4b9920f17d288b63b9af99b37b5c7f9
Fedor Katurov <gotham48@gmail.com>
2019-10-21 13:28:24 +0700

    cleanup

--- src/components/editors/ImageGrid/index.tsx
+++ src/components/editors/ImageGrid/index.tsx
@@ -1,11 +1,9 @@
 import React, { FC, useCallback } from 'react';
 import { SortEnd } from 'react-sortable-hoc';
 import * as styles from './styles.scss';
-import { IFile, INode } from '~/redux/types';
+import { IFile } from '~/redux/types';
 import { IUploadStatus } from '~/redux/uploads/reducer';
-import assocPath from 'ramda/es/assocPath';
 import { moveArrItem } from '~/utils/fn';
-import reject from 'ramda/es/reject';
 import { SortableImageGrid } from '~/components/editors/SortableImageGrid';
 
 interface IProps {
