313c41b56c351cef02b70436b464679e29f4f7ec
Fedor Katurov <gotham48@gmail.com>
2019-10-23 12:08:50 +0700

    limited heroic nodes to images

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -2,6 +2,7 @@ import { USER_ROLES } from '~/redux/auth/constants';
 import { INode } from '~/redux/types';
 import { IUser } from '~/redux/auth/types';
 import path from 'ramda/es/path';
+import { NODE_TYPES } from '~/redux/node/constants';
 
 export const canEditNode = (node: Partial<INode>, user: Partial<IUser>): boolean =>
   path(['role'], user) === USER_ROLES.ADMIN ||
@@ -11,4 +12,6 @@ export const canLikeNode = (node: Partial<INode>, user: Partial<IUser>): boolean
   path(['role'], user) && path(['role'], user) !== USER_ROLES.GUEST;
 
 export const canStarNode = (node: Partial<INode>, user: Partial<IUser>): boolean =>
-  path(['role'], user) && path(['role'], user) === USER_ROLES.ADMIN;
+  node.type === NODE_TYPES.IMAGE &&
+  path(['role'], user) &&
+  path(['role'], user) === USER_ROLES.ADMIN;
