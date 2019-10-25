920a8adaa2e6faed117de93f1c80ed6644a4b8f8
Fedor Katurov <gotham48@gmail.com>
2019-10-25 10:54:46 +0700

    fixed node related

--- src/components/node/NodeRelated/index.tsx
+++ src/components/node/NodeRelated/index.tsx
@@ -2,35 +2,28 @@ import React, { FC } from 'react';
 import * as styles from './styles.scss';
 import { Group } from '~/components/containers/Group';
 import { INode } from '~/redux/types';
-import { getURL } from '~/utils/dom';
-import { withRouter, RouteComponentProps } from 'react-router';
-import { URLS } from '~/constants/urls';
+import { NodeRelatedItem } from '~/components/node/NodeRelatedItem';
 
-type IProps = RouteComponentProps & {
+interface IProps {
   title: string;
   items: Partial<INode>[];
+}
+
+const NodeRelated: FC<IProps> = ({ title, items }) => {
+  return (
+    <Group className={styles.wrap}>
+      <div className={styles.title}>
+        <div className={styles.line} />
+        <div className={styles.text}>{title}</div>
+        <div className={styles.line} />
+      </div>
+      <div className={styles.grid}>
+        {items.map(item => (
+          <NodeRelatedItem item={item} key={item.id} />
+        ))}
+      </div>
+    </Group>
+  );
 };
-
-const NodeRelatedUnconnected: FC<IProps> = ({ title, items, history }) => (
-  <Group className={styles.wrap}>
-    <div className={styles.title}>
-      <div className={styles.line} />
-      <div className={styles.text}>{title}</div>
-      <div className={styles.line} />
-    </div>
-    <div className={styles.grid}>
-      {items.map(item => (
-        <div
-          className={styles.item}
-          key={item.id}
-          style={{ backgroundImage: `url("${getURL({ url: item.thumbnail })}")` }}
-          onClick={() => history.push(URLS.NODE_URL(item.id))}
-        />
-      ))}
-    </div>
-  </Group>
-);
-
-const NodeRelated = withRouter(NodeRelatedUnconnected);
 
 export { NodeRelated };
