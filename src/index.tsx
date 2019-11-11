03558aa7e70dd7a32083b1b3c9b87d6a571c5ebc
Fedor Katurov <gotham48@gmail.com>
2019-11-11 11:46:00 +0700

    cleaned todos

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,3 @@
-/*
-  sortable grid: http://clauderic.github.io/react-sortable-hoc/#/basic-configuration/grid?_k=hjqdj1
-  
-  [BUGS]
-
-  FIXME: flow hangs on empty response
-  
-  [CONTENT]
-
-  TODO: adding photos to comments
-  TODO: adding media to comments
-  TODO: adding media to posts
-  TODO: adding text to posts
-  TODO: covers for posts
-  TODO: wallpapers for posts
-  TODO: changing flow appearance for posts
-
-  TODO: display for all node types
-  TODO: related by tags
-  TODO: sticky left column
-  TODO: adaptive relates count
-
-  TODO: boris page
-  TODO: user profile and settings
-
-  [IMPORT]
-
-  TODO: import users properly
-  TODO: auth using md5 fallback
-  TODO: import files properly
-  TODO: import nodes properly
-  TODO: import comments properly
-  TODO: convert comment content properly
-  TODO: convert post text properly
-
-  [NOT TODAY]
-
-  TODO: swipable image slideshow
-  TODO: notifications
-  TODO: PMs
-  TODO: 
-
-*/
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
@@ -62,29 +19,28 @@ render(
 );
 
 /*
-  - boris with comments (layout)
+- backend: exclude node covers on import
+- profile modal
+- profile editing
+- messages
+- relocate files
+- import videos
+- import graffiti
+- password restore
+- signup?
+- text post can also has songs http://vault48.org/post5052
+
+- notifications ?
+- better node brief update
+
+- social integration (assimilate)
+- comment editing
+
+Done:
+- better dialogs: https://codepen.io/muemue/pen/abbEMMy
+- imagecaching at backend
+- social integration (login, signup)
+- boris with comments (import)
+- boris with comments (layout) 
   
-  - backend: exclude node covers on import
-  - profile modal
-  - profile editing
-  - messages
-  - relocate files
-  - import videos
-  - import graffiti
-  - password restore
-  - signup?
-  - text post can also has songs http://vault48.org/post5052
-  
-  - notifications ?
-  - better node brief update
-  
-  - social integration (assimilate)
-  - comment editing
-  
-  Done:
-  - better dialogs: https://codepen.io/muemue/pen/abbEMMy
-  - imagecaching at backend
-  - social integration (login, signup)
-  - boris with comments (import)
-  
-*/
+  */
