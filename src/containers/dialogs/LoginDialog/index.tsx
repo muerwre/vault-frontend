c2e977e48f665a0090ec0427f5ad956f9cec82dc
Fedor Katurov <gotham48@gmail.com>
2019-11-15 16:16:28 +0700

    fixed login dialog

--- src/containers/dialogs/LoginDialog/index.tsx
+++ src/containers/dialogs/LoginDialog/index.tsx
@@ -1,35 +1,37 @@
-import React, { FC, FormEvent, useCallback, useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { ScrollDialog } from '../ScrollDialog';
-import { IDialogProps } from '~/redux/modal/constants';
-import { useCloseOnEscape } from '~/utils/hooks';
-import { Group } from '~/components/containers/Group';
-import { InputText } from '~/components/input/InputText';
-import { Button } from '~/components/input/Button';
-import { Padder } from '~/components/containers/Padder';
-import * as styles from './styles.scss';
-import { selectAuthLogin } from '~/redux/auth/selectors';
-import * as ACTIONS from '~/redux/auth/actions';
-import { API } from '~/constants/api';
-import { BetterScrollDialog } from '../BetterScrollDialog';
+import React, { FC, FormEvent, useCallback, useEffect, useState } from "react";
+import { connect } from "react-redux";
+import { ScrollDialog } from "../ScrollDialog";
+import { IDialogProps } from "~/redux/modal/constants";
+import { useCloseOnEscape } from "~/utils/hooks";
+import { Group } from "~/components/containers/Group";
+import { InputText } from "~/components/input/InputText";
+import { Button } from "~/components/input/Button";
+import { Padder } from "~/components/containers/Padder";
+import * as styles from "./styles.scss";
+import { selectAuthLogin } from "~/redux/auth/selectors";
+import * as ACTIONS from "~/redux/auth/actions";
+import { API } from "~/constants/api";
+import { BetterScrollDialog } from "../BetterScrollDialog";
 
 const mapStateToProps = selectAuthLogin;
 
 const mapDispatchToProps = {
   userSendLoginRequest: ACTIONS.userSendLoginRequest,
-  userSetLoginError: ACTIONS.userSetLoginError,
+  userSetLoginError: ACTIONS.userSetLoginError
 };
 
-type IProps = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps & IDialogProps & {};
+type IProps = ReturnType<typeof mapStateToProps> &
+  typeof mapDispatchToProps &
+  IDialogProps & {};
 
 const LoginDialogUnconnected: FC<IProps> = ({
   onRequestClose,
   error,
   userSendLoginRequest,
-  userSetLoginError,
+  userSetLoginError
 }) => {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
   const onSubmit = useCallback(
     (event: FormEvent) => {
@@ -40,7 +42,7 @@ const LoginDialogUnconnected: FC<IProps> = ({
   );
 
   const onSocialLogin = useCallback(() => {
-    window.open(API.USER.VKONTAKTE_LOGIN, '', 'width=600,height=400');
+    window.open(API.USER.VKONTAKTE_LOGIN, "", "width=600,height=400");
   }, []);
 
   useEffect(() => {
@@ -59,17 +61,38 @@ const LoginDialogUnconnected: FC<IProps> = ({
 
   return (
     <form onSubmit={onSubmit}>
-      <BetterScrollDialog width={260} error={error} onClose={onRequestClose} footer={buttons}>
+      <BetterScrollDialog
+        width={260}
+        error={error}
+        onClose={onRequestClose}
+        footer={buttons}
+      >
         <Padder>
           <div className={styles.wrap}>
             <Group>
               <h2>РЕШИТЕЛЬНО ВОЙТИ</h2>
 
-              <InputText title="Логин" handler={setUserName} value={username} autoFocus />
-              <InputText title="Пароль" handler={setPassword} value={password} />
+              <InputText
+                title="Логин"
+                handler={setUserName}
+                value={username}
+                autoFocus
+              />
+
+              <InputText
+                title="Пароль"
+                handler={setPassword}
+                value={password}
+                type="password"
+              />
 
               <Group className={styles.buttons}>
-                <Button className={styles.vk} iconLeft="vk" type="button" onClick={onSocialLogin}>
+                <Button
+                  className={styles.vk}
+                  iconLeft="vk"
+                  type="button"
+                  onClick={onSocialLogin}
+                >
                   <span>Вконтакте</span>
                 </Button>
               </Group>
