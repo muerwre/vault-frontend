0e3c7cbfd691c0dafa9d67611a0b6cd297a4b64b
Fedor Katurov <gotham48@gmail.com>
2020-08-27 14:44:48 +0700

    fixed is_loading display

--- src/components/node/NodeNoComments/index.tsx
+++ src/components/node/NodeNoComments/index.tsx
@@ -20,7 +20,7 @@ const NodeNoComments: FC<IProps> = ({ is_loading = false, count = 3 }) => {
   return (
     <Group className={classNames(styles.wrap, { is_loading })}>
       {items}
-      {is_loading && <div className={styles.nothing}>{t(ERRORS.NO_COMMENTS)}</div>}
+      {!is_loading && <div className={styles.nothing}>{t(ERRORS.NO_COMMENTS)}</div>}
     </Group>
   );
 };
