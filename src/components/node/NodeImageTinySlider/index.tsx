c58bf1a3794042cea3cc6256c4bf1646b429bf8f
Fedor Katurov <gotham48@gmail.com>
2020-10-26 12:18:43 +0700

    made simple full-width image container

---
+++ src/components/node/NodeImageTinySlider/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import styles from './styles.module.scss';
+
+interface IProps {}
+
+const NodeImageTinySlider: FC<IProps> = () => <div className={styles.wrap}>Slider :3</div>;
+
+export { NodeImageTinySlider };
