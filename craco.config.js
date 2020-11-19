340ba47d403251001eaed6c8c945d26d82260a1d
Fedor Katurov <gotham48@gmail.com>
2020-11-19 17:38:38 +0700

    fixed Dockerfile to use build instead of dist

--- craco.config.js
+++ craco.config.js
@@ -13,27 +13,27 @@ module.exports = {
     enable: false,
     mode: 'file',
   },
-  // jest: {
-  //   setupTestFrameworkScriptFile: "<rootDir>/src/setupTests.js",
-  //   configure: {
-  //     moduleNameMapper: {
-  //       "^~/(.*)$": "<rootDir>/src/$1",
-  //       "^.+\\.scss$": "identity-obj-proxy"
-  //     },
-  //     snapshotSerializers: ["enzyme-to-json/serializer"],
-  //     moduleFileExtensions: ["js", "json", "ts", "tsx", "jsx", "node"],
-  //     verbose: true,
-  //     roots: ["<rootDir>/src"],
-  //     transform: {
-  //       "^.+\\.tsx?$": "ts-jest",
-  //       "^.+\\.ts?$": "babel-jest",
-  //       "^.+\\.js?$": "ts-jest",
-  //       "^.+\\.jsx?$": "babel-jest"
-  //     },
-  //     preset: "ts-jest/presets/js-with-ts",
-  //     testEnvironment: "node"
-  //   }
-  // },
+  jest: {
+    setupTestFrameworkScriptFile: "<rootDir>/src/setupTests.js",
+    configure: {
+      moduleNameMapper: {
+        "^~/(.*)$": "<rootDir>/src/$1",
+        "^.+\\.scss$": "identity-obj-proxy"
+      },
+      snapshotSerializers: ["enzyme-to-json/serializer"],
+      moduleFileExtensions: ["js", "json", "ts", "tsx", "jsx", "node"],
+      verbose: true,
+      roots: ["<rootDir>/src"],
+      transform: {
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.ts?$": "babel-jest",
+        "^.+\\.js?$": "ts-jest",
+        "^.+\\.jsx?$": "babel-jest"
+      },
+      preset: "ts-jest/presets/js-with-ts",
+      testEnvironment: "node"
+    }
+  },
   plugins: [
     {
       plugin: CracoAlias,
