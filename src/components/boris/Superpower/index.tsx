40f881fdad1eceed369140d46b0eb081680af99e
Fedor Katurov <gotham48@gmail.com>
2021-03-19 18:04:36 +0700

    #23 fixed user detection at superpower

--- src/components/boris/Superpower/index.tsx
+++ src/components/boris/Superpower/index.tsx
@@ -4,13 +4,13 @@ import { selectAuthIsTester, selectUser } from '~/redux/auth/selectors';
 
 interface IProps {}
 
-const Superpower: FC<IProps> = memo(({ children }) => {
+const Superpower: FC<IProps> = ({ children }) => {
   const user = useShallowSelect(selectUser);
   const is_tester = useShallowSelect(selectAuthIsTester);
 
-  if (!user || !is_tester) return null;
+  if (!user.is_user || !is_tester) return null;
 
   return <>{children}</>;
-});
+};
 
 export { Superpower };
