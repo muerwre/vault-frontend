80967ddfac39cd917adbfd55dbceced302a4d15b
Fedor Katurov <gotham48@gmail.com>
2021-03-22 11:13:03 +0700

    #23 fixed lab layout on tablets

--- src/components/lab/LabHead/index.tsx
+++ src/components/lab/LabHead/index.tsx
@@ -3,12 +3,14 @@ import { Group } from '~/components/containers/Group';
 import { Card } from '~/components/containers/Card';
 import { Placeholder } from '~/components/placeholders/Placeholder';
 import { Filler } from '~/components/containers/Filler';
+import styles from './styles.module.scss';
+import { Grid } from '~/components/containers/Grid';
 
 interface IProps {}
 
 const LabHead: FC<IProps> = () => (
-  <Card>
-    <Group horizontal>
+  <Card className={styles.wrap}>
+    <div className={styles.group}>
       <Group horizontal style={{ flex: '0 0 auto' }}>
         <Placeholder width="32px" height={32} />
         <Placeholder width="96px" height={18} />
@@ -25,7 +27,7 @@ const LabHead: FC<IProps> = () => (
       </Group>
 
       <Filler />
-    </Group>
+    </div>
   </Card>
 );
 
