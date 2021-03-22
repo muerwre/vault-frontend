58d5a8ee350a9f3e80e300947ba56c19cc1608a7
Fedor Katurov <gotham48@gmail.com>
2021-03-22 17:46:47 +0700

    #23 added lab tabs

--- src/components/lab/LabHead/index.tsx
+++ src/components/lab/LabHead/index.tsx
@@ -1,32 +1,17 @@
 import React, { FC } from 'react';
-import { Group } from '~/components/containers/Group';
-import { Card } from '~/components/containers/Card';
-import { Placeholder } from '~/components/placeholders/Placeholder';
-import { Filler } from '~/components/containers/Filler';
 import styles from './styles.module.scss';
-import { Grid } from '~/components/containers/Grid';
+import { LabHeadItem } from '~/components/lab/LabHeadItem';
 
 interface IProps {}
 
 const LabHead: FC<IProps> = () => (
   <div className={styles.wrap}>
     <div className={styles.group}>
-      <Group horizontal style={{ flex: '0 0 auto' }}>
-        <Placeholder width="32px" height={32} />
-        <Placeholder width="96px" height={18} />
-      </Group>
-
-      <Group horizontal style={{ flex: '0 0 auto' }}>
-        <Placeholder width="32px" height={32} />
-        <Placeholder width="126px" height={18} />
-      </Group>
-
-      <Group horizontal style={{ flex: '0 0 auto' }}>
-        <Placeholder width="32px" height={32} />
-        <Placeholder width="96px" height={18} />
-      </Group>
-
-      <Filler />
+      <LabHeadItem icon="recent" active>
+        Свежие
+      </LabHeadItem>
+      <LabHeadItem icon="hot">Популярные</LabHeadItem>
+      <LabHeadItem icon="star_full">Важные</LabHeadItem>
     </div>
   </div>
 );
---
+++ src/components/lab/LabHeadItem/index.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react';
+import { Group } from '~/components/containers/Group';
+import { Icon } from '~/components/input/Icon';
+import { Placeholder } from '~/components/placeholders/Placeholder';
+import styles from './styles.module.scss';
+import classNames from 'classnames';
+
+interface IProps {
+  icon: string;
+  isLoading?: boolean;
+  active?: boolean;
+}
+
+const LabHeadItem: FC<IProps> = ({ icon, children, isLoading, active }) => {
+  if (isLoading) {
+    return (
+      <Group horizontal className={styles.item}>
+        <Placeholder width="32px" height={32} />
+        <Placeholder width="96px" height={18} />
+      </Group>
+    );
+  }
+
+  return (
+    <Group horizontal className={classNames(styles.item, { [styles.active]: active })}>
+      <Icon icon={icon} size={24} />
+      <span className={styles.text}>{children}</span>
+    </Group>
+  );
+};
+
+export { LabHeadItem };
