462c27ee897e4b4a9f8633e16400c4f9acb38d00
Fedor Katurov <gotham48@gmail.com>
2021-03-24 17:47:56 +0700

    #23 added heroic lab icon

--- src/components/lab/LabNodeTitle/index.tsx
+++ src/components/lab/LabNodeTitle/index.tsx
@@ -3,6 +3,9 @@ import { INode } from '~/redux/types';
 import styles from './styles.module.scss';
 import { URLS } from '~/constants/urls';
 import { Link } from 'react-router-dom';
+import { Group } from '~/components/containers/Group';
+import { Icon } from '~/components/input/Icon';
+import Tippy from '@tippy.js/react';
 
 interface IProps {
   node: INode;
@@ -12,11 +15,19 @@ const LabNodeTitle: FC<IProps> = ({ node }) => {
   if (!node.title) return null;
 
   return (
-    <div className={styles.wrap}>
+    <Group horizontal className={styles.wrap}>
       <div className={styles.title}>
         <Link to={URLS.NODE_URL(node.id)}>{node.title || '...'}</Link>
       </div>
-    </div>
+
+      {node.is_heroic && (
+        <Tippy content="Важный пост">
+          <div className={styles.star}>
+            <Icon icon="star_full" />
+          </div>
+        </Tippy>
+      )}
+    </Group>
   );
 };
 
