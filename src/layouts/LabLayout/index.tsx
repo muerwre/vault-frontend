8ff18dfd10d05c3422778b106919efa0abc6c7a1
Fedor Katurov <gotham48@gmail.com>
2021-09-14 17:00:06 +0700

    Revert "made fixed-with lab"
    This reverts commit 9d5db844

--- src/layouts/LabLayout/index.tsx
+++ src/layouts/LabLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useCallback, useEffect } from 'react';
 import styles from './styles.module.scss';
 import { Sticky } from '~/components/containers/Sticky';
 import { Container } from '~/containers/main/Container';
@@ -11,12 +11,27 @@ import { LabStats } from '~/containers/lab/LabStats';
 import { useShallowSelect } from '~/utils/hooks/useShallowSelect';
 import { selectLabList } from '~/redux/lab/selectors';
 import { SidebarRouter } from '~/containers/main/SidebarRouter';
+import { Superpower } from '~/components/boris/Superpower';
+import { Toggle } from '~/components/input/Toggle';
+import { usePersistedState } from '~/utils/hooks/usePersistedState';
+import classNames from 'classnames';
 
 interface IProps {}
+
+enum Layout {
+  Fluid = 'fluid',
+  Default = 'default',
+}
 
 const LabLayout: FC<IProps> = () => {
   const { is_loading } = useShallowSelect(selectLabList);
   const dispatch = useDispatch();
+  const [layout, setLayout] = usePersistedState('lab_layout', Layout.Default);
+
+  const isFluid = layout === Layout.Fluid;
+  const toggleLayout = useCallback(() => {
+    setLayout(isFluid ? Layout.Default : Layout.Fluid);
+  }, [setLayout, isFluid]);
 
   useEffect(() => {
     dispatch(labGetList());
@@ -25,7 +40,7 @@ const LabLayout: FC<IProps> = () => {
 
   return (
     <div>
-      <Container>
+      <div className={classNames(styles.container, { [styles.fluid]: isFluid })}>
         <div className={styles.wrap}>
           <Group className={styles.content}>
             <LabHead isLoading={is_loading} />
@@ -38,7 +53,7 @@ const LabLayout: FC<IProps> = () => {
             </Sticky>
           </div>
         </div>
-      </Container>
+      </div>
 
       <SidebarRouter prefix="/lab" isLab />
     </div>
