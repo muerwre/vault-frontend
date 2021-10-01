33c17616e93404255c5bd3e0d463869a7c0b2180
Fedor Katurov <gotham48@gmail.com>
2021-10-01 16:17:46 +0700

    added icons for fake profile stats

--- src/containers/profile/ProfilePageStats/index.tsx
+++ src/containers/profile/ProfilePageStats/index.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from 'react';
 import styles from './styles.module.scss';
+import { Icon } from '~/components/input/Icon';
 
 interface Props {}
 
-const Row: FC<{ count: number; title: string }> = ({ count, title }) => (
+const Row: FC<{ count: number; title: string; icon?: string }> = ({ count, title, icon }) => (
   <div className={styles.row}>
+    {icon && (
+      <div className={styles.icon}>
+        <Icon icon={icon} size={24} />
+      </div>
+    )}
     <div className={styles.counter}>{count > 999 ? '999+' : count}</div>
     <div className={styles.title}>{title}</div>
   </div>
@@ -14,7 +20,8 @@ const ProfilePageStats: FC<Props> = () => (
   <div className={styles.wrap}>
     <Row count={9} title="лет в бункере" />
     <Row count={99} title="постов" />
-    <Row count={99999} title="комментариев" />
+    <Row count={99999} title="комментариев" icon="comment" />
+    <Row count={99} title="лайков" icon="heart_full" />
   </div>
 );
 
