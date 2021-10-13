6e0c9af647ec5c4bbd9595d9a14c09c283724e18
Fedor Katurov <gotham48@gmail.com>
2021-10-13 17:57:42 +0700

    removed unused imports

--- src/containers/main/BottomContainer/index.tsx
+++ src/containers/main/BottomContainer/index.tsx
@@ -1,9 +1,6 @@
 import React, { FC } from 'react';
 import styles from './styles.module.scss';
 import { PlayerBar } from '~/components/bars/PlayerBar';
-import { selectUser } from '~/redux/auth/selectors';
-import { pick } from 'ramda';
-import { connect } from 'react-redux';
 
 type IProps = {};
 
