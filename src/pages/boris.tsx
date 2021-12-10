d2893d1fa362b45a6506dc145b51606bece90c56
Fedor Katurov <gotham48@gmail.com>
2021-12-10 09:14:06 +0700

    fixed lost node context on boris

--- src/pages/boris.tsx
+++ src/pages/boris.tsx
@@ -8,10 +8,13 @@ import { CommentContextProvider } from '~/utils/context/CommentContextProvider';
 import { useImageModal } from '~/utils/hooks/useImageModal';
 import { useNodeComments } from '~/utils/hooks/node/useNodeComments';
 import { useBoris } from '~/utils/hooks/boris/useBoris';
+import { NodeContextProvider } from '~/utils/context/NodeContextProvider';
 
 const BorisPage: VFC = () => {
   const dispatch = useDispatch();
   const {
+    current,
+    is_loading,
     comments,
     comment_count: count,
     is_loading_comments: isLoadingComments,
@@ -20,27 +23,28 @@ const BorisPage: VFC = () => {
   const onShowImageModal = useImageModal();
   const { onLoadMoreComments, onDelete: onDeleteComment } = useNodeComments(696);
   const { title, setIsBetaTester, isTester, stats } = useBoris(comments);
-
   useEffect(() => {
     dispatch(nodeLoadNode(696, 'DESC'));
   }, [dispatch]);
 
   return (
-    <CommentContextProvider
-      comments={comments}
-      count={count}
-      isLoading={isLoadingComments}
-      onShowImageModal={onShowImageModal}
-      onLoadMoreComments={onLoadMoreComments}
-      onDeleteComment={onDeleteComment}
-    >
-      <BorisLayout
-        title={title}
-        setIsBetaTester={setIsBetaTester}
-        isTester={isTester}
-        stats={stats}
-      />
-    </CommentContextProvider>
+    <NodeContextProvider node={current} isLoading={is_loading}>
+      <CommentContextProvider
+        comments={comments}
+        count={count}
+        isLoading={isLoadingComments}
+        onShowImageModal={onShowImageModal}
+        onLoadMoreComments={onLoadMoreComments}
+        onDeleteComment={onDeleteComment}
+      >
+        <BorisLayout
+          title={title}
+          setIsBetaTester={setIsBetaTester}
+          isTester={isTester}
+          stats={stats}
+        />
+      </CommentContextProvider>
+    </NodeContextProvider>
   );
 };
 
