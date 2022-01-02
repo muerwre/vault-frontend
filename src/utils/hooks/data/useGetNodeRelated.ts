7d7afeeaf568202962805ba77ac5c487b8d07b03
Fedor Katurov <gotham48@gmail.com>
2022-01-02 18:07:02 +0700

    fixed node related status

--- src/utils/hooks/data/useGetNodeRelated.ts
+++ src/utils/hooks/data/useGetNodeRelated.ts
@@ -6,12 +6,11 @@ import { useCallback } from 'react';
 import { apiGetNodeRelated } from '~/redux/node/api';
 
 export const useGetNodeRelated = (id?: INode['id']) => {
-  const { data, isValidating: isLoading, mutate } = useSWR<ApiGetNodeRelatedResult>(
-    API.NODE.RELATED(id),
-    () => apiGetNodeRelated({ id })
+  const { data, isValidating, mutate } = useSWR<ApiGetNodeRelatedResult>(API.NODE.RELATED(id), () =>
+    apiGetNodeRelated({ id })
   );
 
   const refresh = useCallback(() => mutate(data, true), [data, mutate]);
 
-  return { related: data?.related, isLoading, refresh };
+  return { related: data?.related, isLoading: isValidating && !data, refresh };
 };
