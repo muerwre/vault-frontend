6e89271ea9f615e482a448099d3cb18ab4779dec
Fedor Katurov <gotham48@gmail.com>
2022-01-04 16:08:08 +0700

    added flow loader to global loader

--- src/hooks/dom/useGlobalLoader.ts
+++ src/hooks/dom/useGlobalLoader.ts
@@ -1,9 +1,12 @@
 import { useEffect } from 'react';
 import { useFlowStore } from '~/store/flow/useFlowStore';
 import { hideLoader } from '~/utils/dom/hideLoader';
+import { useFlowLoader } from '~/hooks/flow/useFlowLoader';
 
 /** simply waits for all data to settle and then show the app */
 export const useGlobalLoader = () => {
+  useFlowLoader();
+
   const flow = useFlowStore();
 
   useEffect(() => {
