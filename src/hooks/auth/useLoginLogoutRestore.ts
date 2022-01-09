8f0ed347908d6253de0472522630584f1fc47f03
Fedor Katurov <gotham48@gmail.com>
2022-01-09 20:52:02 +0700

    fixed toasts

--- src/hooks/auth/useLoginLogoutRestore.ts
+++ src/hooks/auth/useLoginLogoutRestore.ts
@@ -1,16 +1,23 @@
 import { useAuthStore } from '~/store/auth/useAuthStore';
 import { useCallback } from 'react';
 import { apiUserLogin } from '~/api/auth';
+import { showErrorToast } from '~/utils/errors/showToast';
+import { getRandomPhrase } from '~/constants/phrases';
+import { showToastInfo, showToastSuccess } from '~/utils/toast';
 
 export const useLoginLogoutRestore = () => {
   const auth = useAuthStore();
 
-  const logout = useCallback(() => auth.logout(), [auth]);
+  const logout = useCallback(() => {
+    auth.logout();
+    showToastInfo(getRandomPhrase('GOODBYE'));
+  }, [auth]);
 
   const login = useCallback(
     async (username: string, password: string) => {
       const result = await apiUserLogin({ username, password });
       auth.setToken(result.token);
+      showToastInfo(getRandomPhrase('WELCOME'));
       return result.user;
     },
     [auth]
