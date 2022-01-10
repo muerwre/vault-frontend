03ab52f81414a71480ac168a67f7a06acb824651
Fedor Katurov <gotham48@gmail.com>
2022-01-10 10:02:19 +0700

    fixed popeper placement

--- src/components/containers/Sticky/index.tsx
+++ src/components/containers/Sticky/index.tsx
@@ -1,16 +1,14 @@
 import React, { DetailsHTMLAttributes, FC } from 'react';
-import StickyBox from 'react-stickynode';
+import StickyBox from 'react-sticky-box';
 
 interface IProps extends DetailsHTMLAttributes<HTMLDivElement> {
   offsetTop?: number;
 }
 
-const Sticky: FC<IProps> = ({ children, offsetTop = 65 }) => {
-  return (
-    <StickyBox offsetTop={offsetTop} offsetBottom={10}>
-      {children}
-    </StickyBox>
-  );
-};
+const Sticky: FC<IProps> = ({ children, offsetTop = 65 }) => (
+  <StickyBox offsetTop={offsetTop} offsetBottom={10}>
+    {children}
+  </StickyBox>
+);
 
 export { Sticky };
