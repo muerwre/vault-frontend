d7def635e30a669c1ddc3bf03130137ae0ba50eb
Fedor Katurov <gotham48@gmail.com>
2022-01-19 16:22:54 +0700

    (nextjs) added opengraph items

--- src/components/node/NodeHeadMetadata/index.tsx
+++ src/components/node/NodeHeadMetadata/index.tsx
@@ -1,7 +1,10 @@
 import React, { VFC } from 'react';
 
+import Head from 'next/head';
+
 import { PageTitle } from '~/components/common/PageTitle';
 import { useNodeContext } from '~/utils/context/NodeContextProvider';
+import { getURLFromString } from '~/utils/dom';
 import { getPageTitle } from '~/utils/ssr/getPageTitle';
 
 interface NodeHeadMetadataProps {}
@@ -12,6 +15,16 @@ const NodeHeadMetadata: VFC<NodeHeadMetadataProps> = () => {
   return (
     <>
       <PageTitle title={getPageTitle(node.title)} />
+
+      <Head>
+        <meta property="og:title" content={node.title} />
+        <meta property="og:type" content={node.type} />
+        <meta property="og:image" content={getURLFromString(node.thumbnail)} />
+        <meta property="og:image:secure_url" content={getURLFromString(node.thumbnail)} />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:alt" content={node.description} />
+        <meta property="og:description" content={node.description} />
+      </Head>
     </>
   );
 };
