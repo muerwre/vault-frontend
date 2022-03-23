16fe237a4af342df98e39f5763ed940f36cd1a4e
Fedor Katurov <gotham48@gmail.com>
2022-03-23 14:32:18 +0700

    preloading lab after flow

--- src/hooks/dom/useGlobalLoader.ts
+++ src/hooks/dom/useGlobalLoader.ts
@@ -1,17 +1,26 @@
 import { useEffect } from 'react';
 
+import { useAuth } from '~/hooks/auth/useAuth';
 import { useFlowLoader } from '~/hooks/flow/useFlowLoader';
+import { useLab } from '~/hooks/lab/useLab';
 import { useFlowStore } from '~/store/flow/useFlowStore';
 import { hideLoader } from '~/utils/dom/hideLoader';
 
 /** simply waits for all data to settle and then show the app */
 export const useGlobalLoader = () => {
   const { getInitialNodes } = useFlowLoader();
+  const { loadMore: loadLabNodes, nodes: labNodes, isLoading: isLoadingLab } = useLab();
+  const { isUser } = useAuth();
+
   const flow = useFlowStore();
 
   useEffect(() => {
     if (!flow.isRefreshed) {
-      void getInitialNodes();
+      void getInitialNodes().then(() => {
+        if (!isUser || isLoadingLab || labNodes.length) return;
+
+        return loadLabNodes();
+      });
       return;
     }
 
