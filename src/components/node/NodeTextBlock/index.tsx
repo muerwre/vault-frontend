e1131d961d68eceddf105ebe27e9f0ecbe5ec6e2
Fedor Katurov <gotham48@gmail.com>
2022-03-29 17:57:34 +0700

    fixed full node colors and paddings

--- src/components/node/NodeTextBlock/index.tsx
+++ src/components/node/NodeTextBlock/index.tsx
@@ -3,6 +3,7 @@ import React, { FC, useMemo } from 'react';
 import classNames from 'classnames';
 
 import { INodeComponentProps } from '~/constants/node';
+import { useColorGradientFromString } from '~/hooks/color/useColorGradientFromString';
 import markdown from '~/styles/common/markdown.module.scss';
 import { formatTextParagraphs } from '~/utils/dom';
 import { path } from '~/utils/ramda';
@@ -16,9 +17,12 @@ const NodeTextBlock: FC<IProps> = ({ node }) => {
     node,
   ]);
 
+  const background = useColorGradientFromString(node.title, 3, 2);
+
   return (
     <div
       className={classNames(styles.text, markdown.wrapper)}
+      style={{ background }}
       dangerouslySetInnerHTML={{
         __html: content,
       }}
