fc59ad9f80fed18ad097d93172200a6b30b26964
Fedor Katurov <gotham48@gmail.com>
2022-07-31 08:34:08 +0700

    added dudes to clouds

--- src/containers/dialogs/LoginDialog/index.tsx
+++ src/containers/dialogs/LoginDialog/index.tsx
@@ -1,21 +1,21 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useCallback } from "react";
 
-import { LoginDialogButtons } from '~/components/auth/login/LoginDialogButtons';
-import { Group } from '~/components/containers/Group';
-import { Padder } from '~/components/containers/Padder';
-import { BetterScrollDialog } from '~/components/dialogs/BetterScrollDialog';
-import { DialogTitle } from '~/components/dialogs/DialogTitle';
-import { Button } from '~/components/input/Button';
-import { InputText } from '~/components/input/InputText';
-import { Dialog } from '~/constants/modal';
-import { useCloseOnEscape } from '~/hooks';
-import { useAuth } from '~/hooks/auth/useAuth';
-import { useLoginForm } from '~/hooks/auth/useLoginForm';
-import { useOAuth } from '~/hooks/auth/useOAuth';
-import { useShowModal } from '~/hooks/modal/useShowModal';
-import { DialogComponentProps } from '~/types/modal';
+import { LoginDialogButtons } from "~/components/auth/login/LoginDialogButtons";
+import { Group } from "~/components/containers/Group";
+import { Padder } from "~/components/containers/Padder";
+import { BetterScrollDialog } from "~/components/dialogs/BetterScrollDialog";
+import { DialogTitle } from "~/components/dialogs/DialogTitle";
+import { Button } from "~/components/input/Button";
+import { InputText } from "~/components/input/InputText";
+import { Dialog } from "~/constants/modal";
+import { useCloseOnEscape } from "~/hooks";
+import { useAuth } from "~/hooks/auth/useAuth";
+import { useLoginForm } from "~/hooks/auth/useLoginForm";
+import { useOAuth } from "~/hooks/auth/useOAuth";
+import { useShowModal } from "~/hooks/modal/useShowModal";
+import { DialogComponentProps } from "~/types/modal";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 type LoginDialogProps = DialogComponentProps & {};
 
@@ -30,10 +30,13 @@ const LoginDialog: FC<LoginDialogProps> = ({ onRequestClose }) => {
       event.preventDefault();
       showRestoreDialog({});
     },
-    [showRestoreDialog]
+    [showRestoreDialog],
   );
 
-  const { values, errors, handleSubmit, handleChange } = useLoginForm(login, onRequestClose);
+  const { values, errors, handleSubmit, handleChange } = useLoginForm(
+    login,
+    onRequestClose,
+  );
 
   return (
     <form onSubmit={handleSubmit}>
@@ -51,7 +54,7 @@ const LoginDialog: FC<LoginDialogProps> = ({ onRequestClose }) => {
 
                 <InputText
                   title="Логин"
-                  handler={handleChange('username')}
+                  handler={handleChange("username")}
                   value={values.username}
                   error={errors.username}
                   autoFocus
@@ -59,7 +62,7 @@ const LoginDialog: FC<LoginDialogProps> = ({ onRequestClose }) => {
 
                 <InputText
                   title="Пароль"
-                  handler={handleChange('password')}
+                  handler={handleChange("password")}
                   value={values.password}
                   error={errors.password}
                   type="password"
