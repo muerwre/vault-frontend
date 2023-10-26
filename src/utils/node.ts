0605ceb26305fb3830ec4bb9b90aba4e19c2f3aa
Fedor Katurov <gotham48@gmail.com>
2023-10-26 16:48:14 +0600

    let admins star lab nodes

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -4,16 +4,29 @@ import { ICommentGroup, INode } from '~/types';
 import { IUser } from '~/types/auth';
 import { path } from '~/utils/ramda';
 
-export const canEditNode = (node?: Partial<INode>, user?: Partial<IUser>): boolean =>
-  path(['role'], user) === Role.Admin || path(['user', 'id'], node) === path(['id'], user);
+export const canEditNode = (
+  node?: Partial<INode>,
+  user?: Partial<IUser>,
+): boolean =>
+  path(['role'], user) === Role.Admin ||
+  path(['user', 'id'], node) === path(['id'], user);
 
-export const canEditComment = (comment?: Partial<ICommentGroup>, user?: Partial<IUser>): boolean =>
-  path(['role'], user) === Role.Admin || path(['user', 'id'], comment) === path(['id'], user);
+export const canEditComment = (
+  comment?: Partial<ICommentGroup>,
+  user?: Partial<IUser>,
+): boolean =>
+  path(['role'], user) === Role.Admin ||
+  path(['user', 'id'], comment) === path(['id'], user);
 
-export const canLikeNode = (node?: Partial<INode>, user?: Partial<IUser>): boolean =>
-  path(['role'], user) !== Role.Guest;
+export const canLikeNode = (
+  node?: Partial<INode>,
+  user?: Partial<IUser>,
+): boolean => path(['role'], user) !== Role.Guest;
 
-export const canStarNode = (node?: Partial<INode>, user?: Partial<IUser>): boolean =>
-  path(['type'], node) === NODE_TYPES.IMAGE &&
+export const canStarNode = (
+  node?: Partial<INode>,
+  user?: Partial<IUser>,
+): boolean =>
+  (path(['type'], node) === NODE_TYPES.IMAGE || !node?.is_public) &&
   path(['is_promoted'], node) === true &&
   path(['role'], user) === Role.Admin;
