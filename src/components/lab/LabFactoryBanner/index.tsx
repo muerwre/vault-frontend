1d067fb14730f271688c172e985a3c4c244d4765
Fedor Katurov <gotham48@gmail.com>
2023-11-04 13:05:09 +0600

    show random lab banner, lol

--- src/components/lab/LabFactoryBanner/index.tsx
+++ src/components/lab/LabFactoryBanner/index.tsx
@@ -1,26 +1,29 @@
-import React, { FC } from 'react';
+import React, { useRef } from 'react';
+
+import classNames from 'classnames';
 
 import { Group } from '~/components/containers/Group';
 
 import styles from './styles.module.scss';
 
-interface LabFactoryBannerProps {}
+const LabFactoryBanner = () => {
+  const masked = useRef(Math.random() <= 0.5).current;
 
-const LabFactoryBanner: FC<LabFactoryBannerProps> = () => (
-  <div className={styles.banner}>
-    <Group>
-      <div className={styles.title}>Лаборатория!</div>
+  return (
+    <div className={classNames(styles.banner, { [styles.masked]: masked })}>
+      <Group>
+        <div className={styles.title}>Лаборатория!</div>
 
-      <Group className={styles.content}>
-        <p>
-          <strong>
-            Всё, что происходит здесь &mdash; всего лишь эксперимент, о котором
-            не узнает никто за пределами Убежища.
-          </strong>
-        </p>
+        <Group className={styles.content}>
+          <p>
+            <strong>
+              Всё, что происходит здесь &mdash; всего лишь эксперимент, о
+              котором не узнает никто за пределами Убежища.
+            </strong>
+          </p>
+        </Group>
       </Group>
-    </Group>
-  </div>
-);
-
+    </div>
+  );
+};
 export { LabFactoryBanner };
